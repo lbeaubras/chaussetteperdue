4db427f44f8aeee4a75fa4b8bfebee4f5f4d36c8
lbeaubras <32261822+lbeaubras@users.noreply.github.com>
2017-09-25 12:29:55 +0200

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,19 @@
+<?php
+<div id="container"> 
+
+  <div id="header">
+    <div class="headtitle">Les chaussettes égarées</div>
+  </div>
+
+  <div id="menu"> 
+    <ul>
+      <li><a href="#" title="">Accueil</a></li>
+      <li><a href="contact.html" title="">RETROUVEZ</a></li>
+      <li><a href="#" title="">Chaussettes news</a></li>
+      <li><a href="#" title="">Contactez nous</a></li>
+      <li><a href="#" title="">LINKS</a></li>
+
+    </ul>
+  </div>
+  
+  ?>
